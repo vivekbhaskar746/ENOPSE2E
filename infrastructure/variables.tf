fb2064e097477a2a69b64d135e3ed8524d192881
vivek bhaskar <0111cs211202.vivekbhaskar@gmail.com>
2026-02-16 19:17:12 +0000

    configured variables

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -1,26 +1,27 @@
-variable "acm_cert_arn" { type = string }
-variable "db_user" {
-  description = "Database username"
-  default     = "root"
-}
+# variable "acm_cert_arn" { type = string }
+# variable "db_user" {
+#   description = "Database username"
+#   default     = "root"
+# }
 
 variable "db_password" {
   description = "Database password"
+  default = "vivekbhaskar"
   validation {
     condition = length(var.db_password)>=8
     error_message = "db_password must be 8 characters"
   }
 }
 
-variable "blog_backend_image" {
-  description = "Container image for the blog generator backend"
-  type        = string
-}
+# variable "blog_backend_image" {
+#   description = "Container image for the blog generator backend"
+#   type        = string
+# }
 
-variable "blog_frontend_image" {
-  description = "Container image for the blog generator frontend"
-  type        = string
-}
+# variable "blog_frontend_image" {
+#   description = "Container image for the blog generator frontend"
+#   type        = string
+# }
 
 output "db_host" {
   value       = aws_db_instance.postgres.endpoint
