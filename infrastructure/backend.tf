e9ca9b8766e3c2b8fa054a9f1579bf443e05a978
vivek bhaskar <0111cs211202.vivekbhaskar@gmail.com>
2026-02-18 07:23:39 +0000

    fixed fetch rds end point stage

--- infrastructure/backend.tf
+++ infrastructure/backend.tf
@@ -1,9 +1,11 @@
 terraform {
   backend "s3" {
-    bucket         = "enops-tf-state-bucket"
-    key            = "enopse2e/infrastructure/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "terraform-locks"
-    encrypt        = true
+    bucket        = "enops-tf-state-bucket"
+    key           = "infra/terraform.tfstate"
+    region        = "us-east-1"
+    encrypt       = true
+
+    # New way (Terraform ≥ 1.9/1.10): use S3 object locking as a lockfile
+    use_lockfile  = true
   }
 }
