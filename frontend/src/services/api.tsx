3ad9458ccb33eb73ce4ee666058a8cc6898db932
vivek bhaskar <0111cs211202.vivekbhaskar@gmail.com>
2026-02-18 04:24:18 +0000

    fixed api base url

--- frontend/src/services/api.tsx
+++ frontend/src/services/api.tsx
@@ -1,7 +1,4 @@
 import axios from 'axios';
-import dotenv from 'dotenv';
-
-dotenv.config();
 
 const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:9090/api'; // Default fallback
 
